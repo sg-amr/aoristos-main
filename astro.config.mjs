c2d7ad1ff31f144671e11fdc0afab5c0285ea2f9
Team Aoristos <163826486+sg-amr@users.noreply.github.com>
2026-06-20 13:42:37 +0900

    Add site and base URL to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  site: 'https://sg-amr.github.io',
+  base: '/aoristos-main'
   vite: {
     plugins: [tailwindcss()]
   }
